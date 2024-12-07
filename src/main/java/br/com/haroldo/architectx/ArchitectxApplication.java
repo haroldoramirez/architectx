1c3951307a6346050787b7329ca610c1452a2cbb
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2024-12-07 11:54:19 -0300

    first commit

---
+++ src/main/java/br/com/haroldo/architectx/ArchitectxApplication.java
@@ -0,0 +1,13 @@
+package br.com.haroldo.architectx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/haroldo/architectx/ArchitectxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.haroldo.architectx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
