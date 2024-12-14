852283f03e8f62971cec71407574f77bc8a04ddb
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2024-12-14 12:57:25 -0300

    criado pagina home com thymelfeaf e boas praticas deixando desacoplado do js

---
+++ src/main/java/br/com/haroldo/architectx/controllers/HomeController.java
@@ -0,0 +1,71 @@
+package br.com.haroldo.architectx.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.haroldo.architectx.vo.Card;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		
+		List<Card> cards = criarCards();
+		
+		model.addAttribute("cards", cards);
+		
+		return "index";
+		
+	}
+	
+	@GetMapping("form-validation")
+	public String formValidation() {
+		return "formValidation";
+	}
+	
+	private List<Card> criarCards() {
+		
+		List<Card> cards = new ArrayList<>();
+				
+		Card cardFormValidation = Card.builder()
+				.urlImage("/images/coffee.webp")
+				.imageAlt("formValidation")
+				.title("Forms")
+				.subtitle("Frontend inputs validations")
+				.description("Case examples to use Javascript or jQuery for validate form inputs")
+				.urlPage("input-validations")
+				.build();
+		
+		cards.add(cardFormValidation);
+		
+		Card cardFormValidation1 = Card.builder()
+				.urlImage("/images/coffee.webp")
+				.imageAlt("formValidation")
+				.title("Forms 2")
+				.subtitle("Frontend inputs validations 2")
+				.description("Case examples to use Javascript or jQuery for validate form inputs 2")
+				.urlPage("input-validations")
+				.build();
+		
+		cards.add(cardFormValidation1);
+		
+		Card cardFormValidation2 = Card.builder()
+				.urlImage("/images/coffee.webp")
+				.imageAlt("formValidation")
+				.title("Forms 3")
+				.subtitle("Frontend inputs validations 2")
+				.description("Case examples to use Javascript or jQuery for validate form inputs 3")
+				.urlPage("input-validations")
+				.build();
+		
+		cards.add(cardFormValidation2);
+		
+		return cards;
+	}
+
+}
---
+++ src/main/java/br/com/haroldo/architectx/vo/Card.java
@@ -0,0 +1,21 @@
+package br.com.haroldo.architectx.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Card {
+	
+	private String urlImage;
+	private String imageAlt;
+	private String title;
+	private String subtitle;
+	private String description;
+	private String urlPage;
+
+}
